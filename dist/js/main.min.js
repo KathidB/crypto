29ba15e651c75c86fbae47389dd2f068fa7765be
KathidB <Kathidbb@gmail.com>
2022-06-07 14:30:55 +0200

    first commit

--- dist/js/main.min.js
+++ dist/js/main.min.js
@@ -1,78 +1,63 @@
-const input = document.querySelector(".input");
-const button = document.querySelector(".search-btn");
-const cityName = document.querySelector(".wapp-body__city");
-const warning = document.querySelector(".wapp-top__para");
-const photo = document.querySelector(".wapp-body__icon");
-const weather = document.querySelector(".wapp-body__weather");
-const pressure = document.querySelector(".wapp-bot__pressure");
-const temperature = document.querySelector(".wapp-bot__temperature");
-const humidity = document.querySelector(".wapp-bot__humidity");
-const label = document.querySelector("#toggle");
-const wappBg = document.querySelector(".wapp");
-const wappResults = document.querySelector(".wapp-bot__results");
-const wappError = document.querySelector(".wapp-top__para");
+const LINK = "https://api.cryptorank.io/v1/currencies?api_key=";
+const APIKEY = "aa82a291dee8c12608c52b1401f3d6396cb81b1919ac2093f586f4fb0531";
 
-const API_LINK = "https://api.openweathermap.org/data/2.5/weather?q=";
-const API_KEY = "&appid=e42e687500228688860fd7ef2cb67f5d";
-const LANG = "&lang=pl";
-const API_UNITS = "&units=metric";
+const places = document.querySelectorAll("tbody tr td:nth-child(1)");
+const names = document.querySelectorAll("tbody tr td:nth-child(2)");
+const prices = document.querySelectorAll("tbody tr td:nth-child(3)");
+const changes = document.querySelectorAll("tbody tr td:nth-child(4)");
+const marketCap = document.querySelectorAll("tbody tr td:nth-child(5)");
+const volume = document.querySelectorAll("tbody tr td:nth-child(6)");
+const supply = document.querySelectorAll("tbody tr td:nth-child(7)");
 
-async function getWeather() {
-  const city = input.value;
-  const URL = API_LINK + city + API_KEY + API_UNITS + LANG;
-  axios
-    .get(URL)
-    .then((res) => {
-      const status = res.data.weather[0].id;
+let howMany = 10;
 
-      cityName.textContent = res.data.name;
+async function getCrypto() {
+  try {
+    const res = await fetch(LINK + APIKEY);
+    const data = await res.json();
 
-      temperature.textContent = Math.floor(res.data.main.temp) + " °C";
-      weather.textContent = res.data.weather[0].description;
-      humidity.textContent = res.data.main.humidity + " %";
-      pressure.textContent = res.data.main.pressure + ` hPa`;
+    for (let i = 0; i < howMany; i++) {
+      // pozycja crypto
+      places[i].textContent = data.data[i].rank;
+      //nazwy crypto
+      names[i].innerText = `${(names[i] = data.data[i].name)} | ${
+        data.data[i].symbol
+      }`;
+      //ceny crypto
 
-      photo.setAttribute("src", "./dist/img/unknown.png");
+      prices[i].textContent = data.data[i].values.USD.price.toFixed(2) + " USD";
 
-      warning.textContent = "Szukaj miasta";
-      input.value = "";
-
-      // if sprawdzajacy pogodowy status id i definiuje odpowiednią pogode za pomoca obrazka.
-
-      if (status <= 232) {
-        photo.setAttribute("src", "./dist/img/thunderstorm.png");
-      } else if (status <= 321) {
-        photo.setAttribute("src", "./dist/img/drizzle.png");
-      } else if (status <= 531) {
-        photo.setAttribute("src", "./dist/img/rain.png");
-      } else if (status <= 622) {
-        photo.setAttribute("src", "./dist/img/ice.png");
-      } else if (status <= 781) {
-        photo.setAttribute("src", "./dist/img/fog.png");
-      } else if (status === 800) {
-        photo.setAttribute("src", "./dist/img/sun.png");
-      } else if (status <= 804) {
-        photo.setAttribute("src", "./dist/img/cloud.png");
+      // zmiana 24h
+      changes[i].textContent = data.data[i].values.USD.percentChange24h + " %";
+      if (data.data[i].values.USD.percentChange24h > 0) {
+        changes[i].style.color = "#33cc33";
+      } else if ((data.data[i].values.USD.percentChange24h = 0)) {
+        changes[i].style.color = "black";
+      } else {
+        changes[i].style.color = "#ff4d4d";
       }
-    })
-    .catch(() => {
-      warning.textContent = "Wpisz poprawną nazwę miasta";
-    });
+      //market cap
+      marketCap[i].textContent =
+        data.data[i].values.USD.marketCap
+          .toFixed(1)
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "$";
+
+      //wolumen24h
+      volume[i].textContent =
+        data.data[i].values.USD.volume24h
+          .toFixed(1)
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "$";
+
+      // circ supply
+      supply[i].textContent = data.data[i].circulatingSupply
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+  } catch {
+    console.error("ERROR 404");
+  }
 }
 
-// szukanie miasta za pomoca klawisza enter
-
-button.addEventListener("click", getWeather);
-input.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    getWeather();
-  }
-});
-
-//nadawanie klas zwiazanych z dark mode
-label.addEventListener("click", () => {
-  wappBg.classList.toggle("wapp-toggle");
-  input.classList.toggle("wapp-toggle-bg");
-  wappResults.classList.toggle("wapp-toggle-text");
-  wappError.classList.toggle("wapp-toggle-text");
-});
+getCrypto();
